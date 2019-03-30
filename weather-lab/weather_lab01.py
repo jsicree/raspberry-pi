6098d140c7a87760e01187095923b891fd0856b4
Joe Sicree <jsicree@gmail.com>
2019-03-30 19:59:18 -0400

    Added weather_lab01.py

---
+++ weather-lab/weather_lab01.py
@@ -0,0 +1,19 @@
+# ********************************************
+# weather_lab01.py - A simple Python program
+# ********************************************
+
+# Print a message to the screen
+print("Hello World")
+
+# Now, create a variable to hold the message
+message = "Hello World"
+
+# Print the value of the message variable to the screen
+print(message)
+
+# Change the message
+message = "Goodbye! See you soon!"
+
+# Print the new value of the message variable to the screen
+print(message)
+
