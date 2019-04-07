b264bcc26decffc393cc4036d62321b8876d0912
Joe Sicree <jsicree@gmail.com>
2019-04-07 12:16:59 -0400

    Update weather_lab05.py

--- weather-lab/weather_lab05.py
+++ weather-lab/weather_lab05.py
@@ -7,82 +7,38 @@
 from sense_emu import SenseHat
 from time import sleep
 
-# Import a JSON library to create the message
-import json
-
-# Import the requests library to handle the web service call
-import requests
-
-# Import datetime to format the timestamp
-import datetime
-
-# The URL of the RESTful Web Service
-WEATHER_URL = "https://jvypfbgwl0.execute-api.us-east-1.amazonaws.com/default/weatherGateway" 
-
-# Request headers indicating that the request will be JSON
-WEATHER_REQ_HEADERS = {'Content-type': 'application/json'}
-
-# The remote station latitude and longitude
-STATION_LAT_LON = "42.496098, -71.198130"
+from weather_tools import WeatherServiceAdapter
 
 # The station id
 STATION_ID = "WJOE" 
 
-# The readingDate date and time format
-READING_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+# The station location. The WeatherServiceAdapter defines a number of locations to choose from
+MY_LOCATION = WeatherServiceAdapter.STATION_LOC_BHS_FOOTBALL
 
-# *****************************************************
-# A Python function to send weather data to a RESTful
-# web service. 
-# *****************************************************
-def sendReading(stationId, readingDateStr, location, temperature, pressure, humidity):
-
-    # Define a structure containing the weather data.
-    # This is an example of a type of structure called a dict.
-    # It consists of a set of name/value pairs
-    req = {
-        "stationId" : stationId,
-        "temperature" : temperature,
-        "pressure" : pressure,
-        "humidity" : humidity,
-        "readingDate" : readingDateStr,
-        "location" : location
-    }
-
-    # Convert the dict to JSON.
-    data_json = json.dumps(req)
-
-    # Print the JSON request    
-    #print("REST request (JSON):", data_json)
-    
-    # Do a HTTP POST request using the predefined URL and headers, plus
-    # the JSON data structure
-    response = requests.post(WEATHER_URL, data=data_json, headers=WEATHER_REQ_HEADERS)    
-    
-    # Print the JSON response
-    print("REST response (JSON):", response.text)
-
-# Instantiate (create) a variable to access the SenseHat Emulator
+# Create a variable to access the SenseHat Emulator
 sense = SenseHat()
+
+# Create a variable to access the WeatherServiceAdapter, which
+# will allow you to send weather data to the weather dashboard
+weather_service = WeatherServiceAdapter()
+
+print("My weather station id: ", STATION_ID)
+print("My weather station location: ", weather_service.getStationLocation(MY_LOCATION)
 
 # Print a header
 print("#, Temperature, Pressure, Humidity")
 
+# Loop continuously
 while True:
-
-    # Get the current date and time
-    now = datetime.datetime.now()
-    readingDateString = now.strftime(READING_DATE_FORMAT)
-
     # Read temperature, humidity and pressure into local variables
     temperature = sense.temperature
     humidity = sense.humidity
     pressure = sense.pressure
 
     # Print the data to the screen separated by commas
-    print(STATION_ID,",",readingDateString,",",STATION_LAT_LON,",",temperature,",",pressure,",",humidity)
+    print(STATION_ID,",",temperature,",",pressure,",",humidity)
 
-    sendReading(STATION_ID,readingDateString, STATION_LAT_LON, temperature, pressure, humidity)
+    weather_service.sendReading(STATION_ID, MY_LOCATION, temperature, pressure, humidity)
     
     # Sleep for 5 seconds
     sleep(5)
