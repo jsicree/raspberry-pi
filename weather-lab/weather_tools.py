4510dd41e25aba64b2549a6bdde923e47276bd0a
Joe Sicree <jsicree@gmail.com>
2019-04-09 16:48:46 -0400

    Update weather_tools.py

--- weather-lab/weather_tools.py
+++ weather-lab/weather_tools.py
@@ -18,28 +18,79 @@ class WeatherServiceAdapter(object):
     LOC_COORDINATES="coordinates"
 
     # The remote station latitude and longitude
-    STATION_LOC_REC_FIELDS = 0
-    STATION_LOC_BHS_FOOTBALL = 1
-    STATION_LOC_BURLINGTON_COMMON = 2
-    STATION_LOC_SIMONDS_PARK = 3
+    STATION_LOC_ANDOVER = 0
+    STATION_LOC_ARLINGTON = 1
+    STATION_LOC_BEDFORD = 2
+    STATION_LOC_BILLERICA = 3
+    STATION_LOC_BURLINGTON = 4
+    STATION_LOC_CARLISLE = 5
+    STATION_LOC_CONCORD = 6
+    STATION_LOC_LEXINGTON = 7
+    STATION_LOC_LINCOLN = 8
+    STATION_LOC_READING = 9
+    STATION_LOC_SUDBURY = 10
+    STATION_LOC_WALTHAM = 11
+    STATION_LOC_WILMINGTON = 12
+    STATION_LOC_WOBURN = 13
+    
 
     STATION_LOCS = [
         {
-            "name" : "Burlington Rec Fields", 
-            "coordinates" : "42.504514, -71.189193"
+            "name" : "Andover", 
+            "coordinates" : {"lat":"42.657601","lon": "-71.137176"}
         },
         {
-            "name" : "Burlington High School Football Field", 
-            "coordinates" : "42.500309, -71.195582"
+            "name" : "Arlington",             
+            "coordinates" : {"lat":"42.415476","lon": "-71.156852"}
         },
         {
-            "name" : "Burlington Town Common", 
-            "coordinates" : "42.505434, -71.194981"
+            "name" : "Bedford", 
+            "coordinates" : {"lat":"42.491271","lon": "-71.277295"}
         },
         {
-            "name" : "Burlington Simonds Park", 
-            "coordinates" : "42.505579, -71.197056"
-        }        
+            "name" : "Billerica", 
+            "coordinates" : {"lat":"42.558589","lon": "-71.266567"}
+        },
+        {
+            "name" : "Burlington", 
+            "coordinates" : {"lat" : "42.505847", "lon" : "-71.197943"}
+        },
+        {
+            "name" : "Carlisle", 
+            "coordinates" : {"lat":"42.529419","lon": "-71.350845"}
+        },
+        {
+            "name" : "Concord", 
+            "coordinates" : {"lat":"42.460651","lon": "-71.348319"}
+        },
+        {
+            "name" : "Lexington", 
+            "coordinates" : {"lat":"42.447786","lon": "-71.227149"}
+        },
+        {
+            "name" : "Lincoln", 
+            "coordinates" : {"lat":"42.425571","lon": "-71.304208"}
+        },
+        {
+            "name" : "Reading", 
+            "coordinates" : {"lat":"42.525809","lon": "-71.096574"}
+        },
+        {
+            "name" : "Sudbury", 
+            "coordinates" : {"lat":"42.383335","lon": "-71.409973"}
+        },        
+        {
+            "name" : "Waltham", 
+            "coordinates" : {"lat":"42.376546","lon": "-71.234717"}
+        },
+        {
+            "name" : "Wilmington", 
+            "coordinates" : {"lat" : "42.549984", "lon" : "-71.175003"}
+        },
+        {
+            "name" : "Woburn", 
+            "coordinates" : {"lat":"42.478974","lon": "-71.151339"}
+        }
     ]
 
     # The readingDate date and time format
@@ -60,7 +111,7 @@ class WeatherServiceAdapter(object):
             "pressure" : pressure,
             "humidity" : humidity,
             "readingDate" : now.strftime(WeatherServiceAdapter.READING_DATE_FORMAT),
-            "location" : station_loc[WeatherServiceAdapter.LOC_COORDINATES]
+            "location" : station_loc
         }
     
         # Convert the dict to JSON.
@@ -82,3 +133,4 @@ class WeatherServiceAdapter(object):
     
     def getAllStationLocations(self):
         return WeatherServiceAdapter.STATION_LOCS
+    
